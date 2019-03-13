87636c669e3349502bbf93eaac91c12077dfd4d1
Edson Terceros <edson.terceros@avantica.net>
2019-03-13 08:25:16 -0400

    navegacion desde el componente goBack location. Parametros de ActivatedRoute

--- src/app/item-detail/item-detail.component.ts
+++ src/app/item-detail/item-detail.component.ts
@@ -1,5 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Item} from '../shared/item';
+import {ItemService} from '../services/item.service';
+import {ActivatedRoute} from '@angular/router';
+import {Location} from '@angular/common';
 
 @Component({
   selector: 'app-item-detail',
@@ -8,12 +11,20 @@ import {Item} from '../shared/item';
 })
 export class ItemDetailComponent implements OnInit {
 
-  @Input()
   item: Item;
-  constructor() {
+
+  constructor(private itemService: ItemService,
+              private route: ActivatedRoute,
+              private location: Location) {
   }
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    this.item = this.itemService.getItem(id);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
