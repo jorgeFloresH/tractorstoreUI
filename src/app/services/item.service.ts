96c98b1ac103652ceb962e67114adee526cfb2c7
Edson Terceros <edson.terceros@avantica.net>
2019-03-13 07:44:04 -0400

    Single Page Applications

--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -13,4 +13,13 @@ export class ItemService {
   getItems(): Item[] {
     return ITEMS;
   }
+
+  getItem(id: number): Item {
+    return ITEMS.filter(item => item.id === id)[0];
+  }
+
+  getFeaturedItem(): Item {
+    return ITEMS.filter((item) => item.featured)[0];
+  }
+
 }
